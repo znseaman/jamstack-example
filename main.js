a9a2498475685b5e39ecb17e59e6d0fde37a796a
Zach Seaman <znseaman@gmail.com>
2019-10-22 09:35:23 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const listRepos = async username => {
+    const repos = await fetch(
+        `https://api.github.com/users/${username}/repos?type=owner&sort=updated`
+    )
+    .then(res => res.json())
+    .catch(error => console.error);
+
+    const markup = repos.map(repo => `
+        <li>
+            <a href="${repo.html_url}">${repo.name}</a>
+            (⭐️ ${repo.stargazers_count})
+        </li>
+    `).join('');
+
+    const content = document.getElementById('content');
+    content.innerHTML = `<ul>${markup}</ul>`;
+}
+
+listRepos('znseaman');
